977ee2320f3a03fb3c3859b9961e29cb1fe24cfa
koheishingai <uiine.tokyo@gmail.com>
2016-01-17 21:36:58 +0900

    proto完成

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/happy_m', routes.happy_m);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.happy_m = function(req, res){
+  res.render('happy_m', { title: 'Express' });
+};
